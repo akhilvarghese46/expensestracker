d4783fa8dfe1360daa8e3c2a759b5eb31424fbda
Akhil varghese <akhil.varghese@student.griffith.ie>
2021-10-25 13:52:50 +0100

    added text view and and floating button for add income and expenses

--- app/src/main/java/com/example/expensestracker/MainActivity.kt
+++ app/src/main/java/com/example/expensestracker/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.expensestracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var clicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val add_btn: View = findViewById(R.id.add_btn)
+        val add_income: View = findViewById(R.id.add_income)
+        val add_expenses: View = findViewById(R.id.add_expenses)
+        add_btn.setOnClickListener{
+            onAddButtonClick()
+        }
+
+        add_income.setOnClickListener{
+            Toast.makeText(this,"add_income buttom Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        add_expenses.setOnClickListener{
+            Toast.makeText(this,"add_expenses edit buttom Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun onAddButtonClick() {
+        setVisibility(clicked)
+        clicked = !clicked
+    }
+
+    private fun setVisibility(clicked:Boolean) {
+        val add_income: View = findViewById(R.id.add_income)
+        val add_expenses: View = findViewById(R.id.add_expenses)
+        if(!clicked){
+            add_expenses.visibility=View.VISIBLE
+            add_income.visibility=View.VISIBLE
+        }else{
+            add_expenses.visibility=View.INVISIBLE
+            add_income.visibility=View.INVISIBLE
+        }
+
     }
 }
